a310488de2875a4a628cbdbd78960944cb17eb92
WX <42308734@qq.com>
2021-08-25 01:20:34 +0800

    fix:静态部署设置

--- .umirc.ts
+++ .umirc.ts
@@ -1,20 +1,25 @@
 import { defineConfig } from 'dumi';
-
-const repo = 'antd-dumi-ui';
+const env = process.env.NODE_ENV;
 
 export default defineConfig({
-  title: repo,
-  favicon: `/${repo}/images/59959718.jpeg`,
-  logo: `/${repo}/images/59959718.jpeg`,
-  outputPath: 'dist',
+  title: 'antd-dumi-ui',
+  favicon:
+    env === 'development'
+      ? '/antd-dumi-ui/images/59959718.jpeg'
+      : './images/59959718.jpeg',
+  logo:
+    env === 'development'
+      ? '/antd-dumi-ui/images/59959718.jpeg'
+      : './images/59959718.jpeg',
+  outputPath: 'docs-dist',
   mode: 'doc',
   hash: true,
   history: {
     type: 'hash',
   },
   // Because of using GitHub Pages
-  base: `/${repo}/`,
-  publicPath: `/${repo}/`,
+  base: env === 'development' ? '/antd-dumi-ui' : './',
+  publicPath: env === 'development' ? '/antd-dumi-ui/' : './',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   navs: [
     null,
